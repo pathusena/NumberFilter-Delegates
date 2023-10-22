86c44c68cc82a30ec6dabacb533e7e2b86949602
Pathum Senaratna <pathusena@gmail.com>
2023-10-22 09:42:52 +0530

    Initial Create

---
+++ DelegateExample/FilterNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateExample
+{
+    public delegate bool FilterDelegate(int numb);
+    public static class FilterNumber
+    {
+        public static List<int> Filter(List<int> numbers, FilterDelegate filterDelegate) { 
+            List<int> results = new List<int>();
+            foreach (int num in numbers)
+            {
+                if (filterDelegate(num)) { 
+                    results.Add(num);
+                }
+            }
+            return results;
+        }
+
+        public static bool IsEven(int n) { 
+            return n%2 == 0;
+        }
+
+        public static bool IsOdd(int n)
+        {
+            return n%2 != 0;
+        }
+    }
+}
---
+++ DelegateExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using DelegateExample;
+
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+FilterDelegate evenFilter = FilterNumber.IsEven;
+FilterDelegate oddFilter = FilterNumber.IsOdd;
+
+List<int> evenNumbers = FilterNumber.Filter(numbers, evenFilter);
+List<int> oddNumbers = FilterNumber.Filter(numbers, oddFilter);
+
+Console.WriteLine("Even numbers: " + string.Join(", ", evenNumbers));
+Console.WriteLine("Odd numbers: " + string.Join(", ", oddNumbers));
+
+Console.ReadKey();
