5a53624bf7aab4002e2c064e42e32038356a986a
Pathum Senaratna <pathusena@gmail.com>
2023-10-22 14:22:30 +0530

    Main method changes

--- DelegateExample/Program.cs
+++ DelegateExample/Program.cs
@@ -1,14 +1,20 @@
 ﻿using DelegateExample;
 
-List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+public class Program {
+    private static void Main(string[] args)
+    {
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-FilterDelegate evenFilter = FilterNumber.IsEven;
-FilterDelegate oddFilter = FilterNumber.IsOdd;
+        FilterDelegate evenFilter = FilterNumber.IsEven;
+        FilterDelegate oddFilter = FilterNumber.IsOdd;
 
-List<int> evenNumbers = FilterNumber.Filter(numbers, evenFilter);
-List<int> oddNumbers = FilterNumber.Filter(numbers, oddFilter);
+        List<int> evenNumbers = FilterNumber.Filter(numbers, evenFilter);
+        List<int> oddNumbers = FilterNumber.Filter(numbers, oddFilter);
 
-Console.WriteLine("Even numbers: " + string.Join(", ", evenNumbers));
-Console.WriteLine("Odd numbers: " + string.Join(", ", oddNumbers));
+        Console.WriteLine("Even numbers: " + string.Join(", ", evenNumbers));
+        Console.WriteLine("Odd numbers: " + string.Join(", ", oddNumbers));
 
-Console.ReadKey();
+        Console.ReadKey();
+    }
+
+}
